3eb5def2f8cea71a736be05733dee3193177b3f8
Nicolas Tamalu <nicotamalu@gmail.com>
2023-04-19 22:56:31 -0300

    feat: add SignUp Factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,18 @@
+import { SignUpController } from '../../presentation/controllers/signup/signup'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
+import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
+import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+
+export const makeSignUpControler = (): SignUpController => {
+  const emailValidatorAdaptar = new EmailValidatorAdapter()
+  const salt = 12
+  const encrypter = new BcryptAdapter(salt)
+  const addAccountRepository = new AccountMongoRepository()
+  const addAccount = new DbAddAccount(encrypter, addAccountRepository)
+  const signUpController = new SignUpController(
+    emailValidatorAdaptar,
+    addAccount
+  )
+  return signUpController
+}
