53314f9ca14f693025247e52db07e7c58b2ca26a
tjapa <nicotamalu@gmail.com>
2023-10-14 00:05:47 -0300

    test: ensure DbAuthentication returns a token on success

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -131,4 +131,10 @@ describe('DbAuthentication Usecase', () => {
     const promise = sut.auth(makeFakeAuthentication())
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should throw if TokenGenerator throws', async () => {
+    const { sut } = makeSut()
+    const accessToken = await sut.auth(makeFakeAuthentication())
+    expect(accessToken).toBe('any_token')
+  })
 })
