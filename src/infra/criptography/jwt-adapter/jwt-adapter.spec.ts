67c770da93728e18ddaade11648423d45a0e2c31
tjapa <nicotamalu@gmail.com>
2023-10-15 19:23:06 -0300

    refactor: remove sut creation to a factory method

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -8,22 +8,27 @@ jest.mock('jsonwebtoken', () => ({
 })
 )
 
+const secret = 'any_secret'
+const makeSut = (): JwtAdapter => {
+  return new JwtAdapter(secret)
+}
+
 describe('Jwt Adapter', () => {
   test('Should call sign with correct values', async () => {
-    const sut = new JwtAdapter('secret')
+    const sut = makeSut()
     const signSpy = jest.spyOn(jwt, 'sign')
     await sut.encrypt('any_id')
-    expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
+    expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'any_secret')
   })
 
   test('Should return a token on sign values', async () => {
-    const sut = new JwtAdapter('secret')
+    const sut = makeSut()
     const accessToken = await sut.encrypt('any_id')
     expect(accessToken).toBe('any_token')
   })
 
   test('Should throw if sign throws', async () => {
-    const sut = new JwtAdapter('secret')
+    const sut = makeSut()
     jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw new Error() })
     const promise = sut.encrypt('any_value')
     await expect(promise).rejects.toThrow()
