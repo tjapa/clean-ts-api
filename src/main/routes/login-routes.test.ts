a8fdfdcdae37b48e28024a7b97b4717ea5a6fc4c
tjapa <nicotamalu@gmail.com>
2023-10-19 23:04:26 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -49,5 +49,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'any_email@gmail.com',
+          password: '123'
+        })
+        .expect(401)
+    })
   })
 })
