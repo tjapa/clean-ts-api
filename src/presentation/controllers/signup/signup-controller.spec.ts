be6c71032feac28a8079b5c39c9885fb35aad59c
tjapa <nicotamalu@gmail.com>
2023-10-28 23:33:50 -0300

    test: ensure SignUpController reutrns 500 if Authentication throws

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -125,4 +125,13 @@ describe('SignUp Controller', () => {
       password: makeFakeRequest().body.password
     })
   })
+
+  test('Should return 500 if Authentication throws', async () => {
+    const { sut, authenticationStub } = makeSut()
+    jest.spyOn(authenticationStub, 'auth').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
+  })
 })
