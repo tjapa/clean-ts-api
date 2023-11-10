274f8d6e085a79b4aa20336d390e99b7b5385ac4
tjapa <nicotamalu@gmail.com>
2023-11-10 00:03:34 -0300

    refactor: move AuthMiddleware dependencies to a protocols file

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,8 +1,10 @@
-import { AccountModel } from '../../domain/models/account'
-import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
+import {
+  HttpRequest,
+  LoadAccountByToken,
+  AccountModel
+} from './auth-middleware-protocols'
 import { AccessDeniedError } from '../errors'
 import { forbidden, ok, serverError } from '../helpers/http/http-helper'
-import { HttpRequest } from '../protocols'
 import { AuthMiddleware } from './auth-middleware'
 
 const makeFakeAccount = (): AccountModel => ({
--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,7 +1,10 @@
-import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
+import {
+  HttpRequest,
+  HttpResponse,
+  LoadAccountByToken
+} from './auth-middleware-protocols'
 import { AccessDeniedError } from '../errors'
 import { forbidden, ok, serverError } from '../helpers/http/http-helper'
-import { HttpRequest, HttpResponse } from '../protocols'
 
 export class AuthMiddleware implements AuthMiddleware {
   constructor (
