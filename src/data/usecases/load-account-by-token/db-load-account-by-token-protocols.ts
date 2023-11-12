5d6d92c0e42b17ac1e941f1c52a45b363e886ca5
tjapa <nicotamalu@gmail.com>
2023-11-12 19:16:43 -0300

    feat: ensure DbLoadAccountByToken calls Decrypter with correct value

---
+++ src/data/usecases/load-account-by-token/db-load-account-by-token-protocols.ts
@@ -0,0 +1,3 @@
+export * from '../../protocols/criptography/decrypter'
+export { LoadAccountByToken } from '../../../domain/usecases/load-account-by-token'
+export { AccountModel } from '../add-account/db-add-account-protocols'
