cd8f80ac54c18a04faf06f4e2ee4ac50827f1ef8
tjapa <nicotamalu@gmail.com>
2023-11-26 23:25:15 -0300

    fix: dont import map files in routes

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -6,8 +6,13 @@ export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
   readdirSync(path.join(__dirname, '/../routes'))
-    .filter((file) => !file.includes('.test.') && !file.includes('.spec.'))
+    .filter(
+      (file) =>
+        !file.includes('.test.') &&
+        !file.includes('.spec.') &&
+        !file.endsWith('.map')
+    )
     .map(async (file) => {
-      ; (await import(`../routes/${file}`)).default(router)
+      ;(await import(`../routes/${file}`)).default(router)
     })
 }
