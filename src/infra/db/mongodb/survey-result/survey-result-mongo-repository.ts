101464629c775f6304736a35697cd57c3de8049b
tjapa <nicotamalu@gmail.com>
2023-12-07 20:28:11 -0300

    feat: ensure SurveyResultMongoRepository insert new surveyResult if there is no data with same accountId and surveyId

---
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -0,0 +1,22 @@
+import { SaveSurveyResultModel } from '@/domain/usecases/save-survey-result'
+import { SurveyResultModel } from '@/domain/models/survey-result'
+import { SaveSurveyResultRepository } from '@/data/protocols/db/survey/save-survey-result-repository'
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+
+export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
+  async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+    const surveyResultCollection =
+      await MongoHelper.getCollection('surveysResults')
+    const surveyResult = await surveyResultCollection.findOneAndUpdate(
+      { surveyId: data.surveyId, accountId: data.accountId },
+      {
+        $set: {
+          answer: data.answer,
+          date: data.date
+        }
+      },
+      { upsert: true, returnDocument: 'after' }
+    )
+    return MongoHelper.map(surveyResult.value)
+  }
+}
