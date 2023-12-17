2ec6bb33be686cd76584e423de9d1a10c26ce2be
tjapa <nicotamalu@gmail.com>
2023-12-17 18:05:01 -0300

    feat: add routes to swagger

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,21 +1,27 @@
-import { loginPath } from './paths/login-path'
-import { accountSchema } from './schemas/account-schema'
-import { loginParamsSchema } from './schemas/login-params-schema'
+import paths from './paths'
+import components from './components'
+import schemas from './schemas'
 
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node API',
-    description: 'API de enquetes',
+    description: 'API do curso do Mango para realizar enquetes entre programadores',
     version: '1.0.0'
   },
-  servers: [{ url: '/api' }],
-  tags: [{ name: 'Login' }],
-  paths: {
-    '/login': loginPath
+  license: {
+    name: 'GPL-3.0-or-later',
+    url: 'https://spdx.org/licenses/GPL-3.0-or-later.html'
   },
-  schemas: {
-    account: accountSchema,
-    loginParams: loginParamsSchema
-  }
+  servers: [{
+    url: '/api'
+  }],
+  tags: [{
+    name: 'Login'
+  }, {
+    name: 'Enquete'
+  }],
+  paths,
+  schemas,
+  components
 }
