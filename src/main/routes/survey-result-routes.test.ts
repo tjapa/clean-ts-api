01e3b49c23525e1d70fa666ef4d4a0757a7071f0
tjapa <nicotamalu@gmail.com>
2023-12-25 16:15:37 -0300

    feat: ensure load surver returns 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -81,5 +81,28 @@ describe('Survey Result Routes', () => {
     test('Should return 403 on load survey result without accessToken', async () => {
       await request(app).get('/api/surveys/any_id/results').expect(403)
     })
+
+    test('Should return 200 on load survey result with valid accessToken', async () => {
+      const res = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [
+          {
+            answer: 'Answer1',
+            image: 'http://image-name.com'
+          },
+          {
+            answer: 'Answer2'
+          }
+        ],
+        date: new Date()
+      })
+      const surveyId = res.insertedId.toHexString()
+      const accessToken = await makeAccessToken()
+
+      await request(app)
+        .get(`/api/surveys/${surveyId}/results`)
+        .set('x-access-token', accessToken)
+        .expect(200)
+    })
   })
 })
