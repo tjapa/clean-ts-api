39fe1ce6c772c6d0984009014a39743275cc5ab5
tjapa <nicotamalu@gmail.com>
2023-12-25 16:19:10 -0300

    feat: docs add load survey result path

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -1,20 +1,25 @@
 export const surveyResultPath = {
   put: {
-    security: [{
-      apiKeyAuth: []
-    }],
+    security: [
+      {
+        apiKeyAuth: []
+      }
+    ],
     tags: ['Enquete'],
     summary: 'API para criar a resposta de uma enquete',
-    description: 'Essa rota só pode ser executada por **usuários autenticados**',
-    parameters: [{
-      in: 'path',
-      name: 'surveyId',
-      description: 'ID da enquete a ser respondida',
-      required: true,
-      schema: {
-        type: 'string'
+    description:
+      'Essa rota só pode ser executada por **usuários autenticados**',
+    parameters: [
+      {
+        in: 'path',
+        name: 'surveyId',
+        description: 'ID da enquete a ser respondida',
+        required: true,
+        schema: {
+          type: 'string'
+        }
       }
-    }],
+    ],
     requestBody: {
       required: true,
       content: {
@@ -25,6 +30,49 @@ export const surveyResultPath = {
         }
       }
     },
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  },
+  get: {
+    security: [
+      {
+        apiKeyAuth: []
+      }
+    ],
+    tags: ['Enquete'],
+    summary: 'API para consultar o resultado de uma enquete',
+    description:
+      'Essa rota só pode ser executada por **usuários autenticados**',
+    parameters: [
+      {
+        in: 'path',
+        name: 'surveyId',
+        description: 'ID da enquete a ser respondida',
+        required: true,
+        schema: {
+          type: 'string'
+        }
+      }
+    ],
     responses: {
       200: {
         description: 'Sucesso',
