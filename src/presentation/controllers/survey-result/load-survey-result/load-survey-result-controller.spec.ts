41080183fec64084eda25347f156ab90decc96d8
tjapa <nicotamalu@gmail.com>
2023-12-25 15:42:16 -0300

    test: ensure LoadSurveyResultController calls loadSurveyResult with correct id

---
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
@@ -0,0 +1,28 @@
+import { LoadSurveyResultController } from './load-survey-result-controller'
+import { HttpRequest } from './load-survey-result-controller-protocols'
+import MockDate from 'mockdate'
+import { mockLoadSurveyById } from '@/presentation/test'
+
+const mockRequest = (): HttpRequest => ({
+  params: {
+    surveyId: 'any_id'
+  }
+})
+
+describe('LoadSurveyResult Controller', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  test('Should call LoadSurveyById with correct value', async () => {
+    const loadSurveyByIdStub = mockLoadSurveyById()
+    const sut = new LoadSurveyResultController(loadSurveyByIdStub)
+    const loadSpy = jest.spyOn(loadSurveyByIdStub, 'loadById')
+    await sut.handle(mockRequest())
+    expect(loadSpy).toHaveBeenCalledWith('any_id')
+  })
+})
